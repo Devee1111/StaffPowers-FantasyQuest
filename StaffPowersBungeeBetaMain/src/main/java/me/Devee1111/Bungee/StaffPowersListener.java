72c08eac8787838ae91a3ef4aedac5e5428e8f7d
Devee1111 <Devee1111@gmail.com>
2019-07-20 23:32:03 -0600

    Fix called event for staff notifications.

--- StaffPowersBungeeBetaMain/src/main/java/me/Devee1111/Bungee/StaffPowersListener.java
+++ StaffPowersBungeeBetaMain/src/main/java/me/Devee1111/Bungee/StaffPowersListener.java
@@ -4,8 +4,8 @@ import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
-import net.md_5.bungee.api.event.ServerConnectEvent;
-import net.md_5.bungee.api.event.ServerDisconnectEvent;
+import net.md_5.bungee.api.event.PlayerDisconnectEvent;
+import net.md_5.bungee.api.event.PostLoginEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
 
@@ -18,7 +18,7 @@ public class StaffPowersListener  implements Listener {
 	private StaffPowers inst = StaffPowers.getInstance();
 	
 	@EventHandler
-	public void onPlayerJoin(ServerConnectEvent e) {
+	public void onPlayerJoin(PostLoginEvent e) {
 		ProxiedPlayer p = e.getPlayer();
 		if(p.hasPermission("staff.power.onjoinleave.alert")) {
 			if(inst.config.getBoolean("options.onjoinleave.enabled.join") == true) {
@@ -34,7 +34,7 @@ public class StaffPowersListener  implements Listener {
 	}
 	
 	@EventHandler
-	public void onPlayerLeave(ServerDisconnectEvent e) {
+	public void onPlayerLeave(PlayerDisconnectEvent e) {
 		ProxiedPlayer p = e.getPlayer();
 		if(p.hasPermission("staff.power.onjoinleave.alert")) {
 			if(inst.config.getBoolean("options.onjoinleave.enabled.leave") == true) {
